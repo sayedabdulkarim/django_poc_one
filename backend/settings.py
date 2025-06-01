9042aa4b86370ba0654859a1571be9d1c5d315c8
Sayed Abdul <sakarim9124@gmail.com>
2025-06-01 15:12:30 +0530

    Add user authentication to Todo app; restrict access to user-specific todos

--- backend/settings.py
+++ backend/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- todo/views.py
+++ todo/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Todo
 
+@login_required
 def index(request):
-    todos = Todo.objects.all()
+    # Only show todos for the logged-in user
+    todos = Todo.objects.filter(user=request.user)
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
-            Todo.objects.create(title=title)
+            Todo.objects.create(title=title, user=request.user)
         return redirect('/')
     return render(request, 'index.html', {'todos': todos})
 
+@login_required
 def complete(request, todo_id):
-    todo = Todo.objects.get(id=todo_id)
+    # Only allow users to complete their own todos
+    todo = Todo.objects.get(id=todo_id, user=request.user)
     todo.completed = True
     todo.save()
     return redirect('/')
 
+@login_required
 def delete(request, todo_id):
-    todo = Todo.objects.get(id=todo_id)
+    # Only allow users to delete their own todos
+    todo = Todo.objects.get(id=todo_id, user=request.user)
     todo.delete()
     return redirect('/')
